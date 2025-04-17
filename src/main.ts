bf02fe9c439022573235a196f733052d8d1e4344
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-04-17 11:47:27 +0200

    ADD: Snack 1. Type narrowing

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log("ciao")
+//Snack 1
+let datoApi: unknown;
+if (typeof datoApi === "string") {
+  console.log(datoApi.toUpperCase())
+} else if (typeof datoApi === "number") {
+  console.log(datoApi * 2)
+} else if (typeof datoApi === "boolean") {
+  console.log(datoApi ? "si" : "no")
+}
+console.log("Tipo non supportato")
